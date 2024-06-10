d03d8e7d179a1f0f8b3b119a72e6201cd8b1f22a
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-06-10 15:50:06 -0300

    feat: adiciona listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConsumingAPIService } from '../../services/consuming-api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  products: any[] = [];
+  currentPage = 1;
+  pageSize = 10;
+  totalPages = 0;
+  totalItems = 0;
 
+  constructor(private apiService: ConsumingAPIService) {}
+
+  ngOnInit(): void {
+    this.loadPage(this.currentPage);
+  }
+
+  loadPage(page: number): void {
+    this.apiService.getDadosDaApi(page, this.pageSize).subscribe(response => {
+      this.products = response.data.content;
+      this.totalPages = response.data.totalPages;
+      this.totalItems = response.data.totalElements;
+    });
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadPage(this.currentPage);
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadPage(this.currentPage);
+    }
+  }
 }
