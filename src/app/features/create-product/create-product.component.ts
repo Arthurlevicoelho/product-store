7ac75acdb9baf79d72800afc213417b372c0f29f
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-06-13 17:46:53 -0300

    feat: adiciona campos ao formulário de criação de produto

--- src/app/features/create-product/create-product.component.ts
+++ src/app/features/create-product/create-product.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent {
+
+  productForm = new FormGroup({
+    flavor: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30)
+      ]
+    }),
+    availableQuantity: new FormControl<number | null>(null),
+
+    price: new FormControl<number | null>(null, {
+      validators: [
+        Validators.required
+      ]
+    }),
+    description: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(100)
+      ]
+    })
+  });
+
+  onSubmit() {
+    this.productForm.controls.flavor.value;
+    this.productForm.controls.availableQuantity.value;
+    this.productForm.controls.price.value;
+    this.productForm.controls.description.value;
+  }
 
 }
